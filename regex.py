7aa9aaeee7355ebaa0f8ad0e1d20e3acdeef8bba
fajarmuslim <fajarfm123456@gmail.com>
2019-04-20 19:37:21 +0700

    asdf

--- regex.py
+++ regex.py
@@ -1,4 +1,23 @@
 import sys
 
 s = sys.argv[1:] 
-print(s[0:])
+s1 = s[0]
+s2 = s1.split()
+pattern = "^"
+for i in range(len(s2)):
+    pattern = pattern + s2[i] + "." + "*" 
+
+
+#import library regex
+import re
+
+#baca db
+import pandas as pd
+db = pd.read_csv("db.csv")
+
+#mengubah menjadi array 2D
+db1 = db.values
+for i in range(0,len(db)-1):
+    x = re.findall(pattern,db1[i][0])
+    if(x):
+        print(db1[i][1])

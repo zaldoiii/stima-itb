3c6d6d51b15a0250cc435a364497d4e3d1902cb9
fajarmuslim <fajarfm123456@gmail.com>
2019-04-21 07:49:28 +0700

    a

--- regex.py
+++ regex.py
@@ -7,8 +7,8 @@ print(s)
 s2 = s1.split()
 
 pattern = "^"
-for i in range(len(s2)):
-    pattern = pattern + s2[i] + "." + "*" 
+for i in range(1,len(sys.argv)):
+    pattern = pattern + sys.argv[i] + "." + "*" 
 
 print(pattern)
 
